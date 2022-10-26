e46ba1a55812a5c5c461bc1a74ac0528b40a4479
niexiaojiang <2570040683@qq.com>
2022-10-26 16:51:23 +0800

    状态码的封装以及相应数据结的定义

--- src/utils/resp.go
+++ src/utils/resp.go
@@ -24,3 +24,19 @@ func StatusText(code ResCode) string {
 	}
 	return recodeText[UnKnowError]
 }
+
+// RespMsg : 响应数据结构
+type RespMsg struct {
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
+// NewRespMsg : 生成response对象
+func NewRespMsg(code int, msg string, data interface{}) *RespMsg {
+	return &RespMsg{
+		Code: code,
+		Msg:  msg,
+		Data: data,
+	}
+}
