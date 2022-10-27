e6b3a22b868f8b31513c4db29e8af17beb9e5017
niexiaojiang <2570040683@qq.com>
2022-10-27 14:55:49 +0800

    修改为ResCode为方法;同时把返回的数据结构单独放在一个文件里。

--- src/utils/resp.go
+++ src/utils/resp.go
@@ -1,30 +1,5 @@
 package utils
 
-type ResCode int
-
-// 业务逻辑状态码
-const (
-	LoginState ResCode = iota
-
-	// UnKnowError 未知错误
-	UnKnowError
-)
-
-//业务逻辑状态信息描述
-var recodeText = map[ResCode]string{
-	LoginState:  "请登录",
-	UnKnowError: "error",
-}
-
-// StatusText 返回状态码的文本。如果代码为空或未知状态码则返回error
-func StatusText(code ResCode) string {
-	msg, ok := recodeText[code]
-	if ok {
-		return msg
-	}
-	return recodeText[UnKnowError]
-}
-
 // RespMsg : 响应数据结构
 type RespMsg struct {
 	Code int         `json:"code"`
